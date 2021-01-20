3a90774900bbc16d8b20ed9a1b8bbe3585b1af53
Tanner Quesenberry <tanner.quesenberry@gmail.com>
2021-01-19 16:12:31 -0800

    Created list_record layout and FeedAdapter for custom listView presentation

--- app/src/main/java/com/tannerquesenberry/top10downloader/MainActivity.kt
+++ app/src/main/java/com/tannerquesenberry/top10downloader/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.tannerquesenberry.top10downloader
 
+import android.content.Context
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import java.net.URL
+import kotlin.properties.Delegates
 
 class FeedEntry {
     var name: String = ""
@@ -25,21 +29,36 @@ class FeedEntry {
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
+    // Lazy means values are only updated when they are first accessed
+    private val downloadData by lazy { DownloadData(this, findViewById(R.id.xmlListView)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate called")
-        val downloadData = DownloadData()
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
 
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        downloadData.cancel(true)
+    }
+
     // Kotlin way of static keyword
     companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+        private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun doInBackground(vararg params: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${params[0]}")
@@ -55,6 +74,12 @@ class MainActivity : AppCompatActivity() {
 //                Log.d(TAG, "onPostExecute: parameter is $result")
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
+
+//                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
+//                propListView.adapter = arrayAdapter
+
+                val feedAdapter = FeedAdapter(propContext, R.layout.list_record, parseApplications.applications)
+                propListView.adapter = feedAdapter
             }
 
             private fun downloadXML(urlPath: String?): String {
